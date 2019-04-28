0cd1bb86a6b05edec8c968d7b45a06b8c22e3e4a
root <root@Julius.localdomain>
2019-04-28 18:02:56 +0200

    setting up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  #Set the index page / root url
+  root 'pages#home'
+  get 'pages/about'
+  get 'pages/home'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
